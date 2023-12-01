457e0af66fe88f2a5856a7a73a9ee8b6465586dd
Noemi Calizaya <calizayanoemiisabel@gmail.com>
2023-12-01 15:08:25 -0400

    change

--- src/App.java
+++ src/App.java
@@ -3,6 +3,7 @@ import javax.swing.SwingUtilities;
 import controller.MainController;
 
 public class App {
+    //Archivo principal
     public static void main(String[] args) throws Exception {
         Runnable run = () -> {
             new MainController();
