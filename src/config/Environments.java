276148cf3f178503938fe8a7b498a118f8b8de46
Noemi Calizaya <calizayanoemiisabel@gmail.com>
2023-12-01 15:03:02 -0400

    project init

---
+++ src/config/Environments.java
@@ -0,0 +1,6 @@
+package config;
+
+public class Environments {
+    public static String lag = "en";
+    public static String pathFile = "/Users/NOEMI/Desktop/Auxiliatura SIS211/TEMARIO/progra2_examples-master/tema4/example1/data/user.txt";
+}
